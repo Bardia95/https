bfa0d4eeaa68de68be9adf8cfa22b3a5b16c616e
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2018-06-11 11:20:34 -0400

    finished 4

--- step4.js
+++ step4.js
@@ -1,1 +1,31 @@
-step4.js
+var https = require('https');
+
+function getHTML (options, callback) {
+  var customOptions = {
+    host: options.host,
+    path: options.path
+  };
+
+  https.get(options, function (response) {
+  // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+    // Console.log each chunk of data as it is received, concat with \n
+    response.on('data', function (data) {
+      console.log(data, '\n');
+    });
+    response.on('end', function() {
+      callback();
+    });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
